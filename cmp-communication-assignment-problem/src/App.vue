88a920daa90e008eed1e700c0b1cf4b2008a9cb7
Bao Nguyen <98855989+kalaichibun97@users.noreply.github.com>
2024-07-28 17:40:12 +0700

    Commit assignment 6

---
+++ cmp-communication-assignment-problem/src/App.vue
@@ -0,0 +1,42 @@
+<script>
+
+export default {
+  name: "App",
+  data() {
+    return {
+      user: {
+        name: "John Doe",
+        age: 30
+      },
+    }
+  },
+  methods: {
+    setUserData(name, age) {
+      this.user = {
+        name,
+        age: +age
+      }
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <active-user :username="user.name" :age="user.age"></active-user>
+    <user-data @set-data="setUserData"></user-data>
+  </div>
+</template>
+
+<style>
+html {
+  font-family: Arial, sans-serif;
+}
+section {
+  margin: 2rem auto;
+  max-width: 40rem;
+  border-radius: 12px;
+  border: 1px solid #ccc;
+  padding: 1rem;
+}
+</style>
---
+++ cmp-communication-assignment-problem/src/components/ActiveUser.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  name: "ActiveUser",
+  props: {
+    username: {
+      type: String,
+      required: true
+    },
+    age: {
+      type: Number,
+      required: true
+    }
+  }
+}
+</script>
+
+<template>
+  <section>
+    <h2>{{ username }}</h2>
+    <h3>{{ age }} years</h3>
+  </section>
+</template>
+
+<style>
+</style>
